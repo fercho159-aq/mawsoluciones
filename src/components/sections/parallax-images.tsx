2de9a149b00d15938f0544f0bb92ec45455e0aaf
Fernando Trejo <fernandotrejo159@gmail.com>
2025-11-02 08:43:28 +0000

    puedes ajustarlo para que el celular llegue hasta la izquierda de la pan

--- src/components/sections/parallax-images.tsx
+++ src/components/sections/parallax-images.tsx
@@ -19,13 +19,15 @@ const ParallaxImages = () => {
   // Phone Animation
   const phoneScale = useTransform(scrollYProgress, [0.4, 0.7], [0.5, 1]);
   const phoneOpacity = useTransform(scrollYProgress, [0.5, 0.65], [0, 1]);
-  const phoneY = useTransform(scrollYProgress, [0.4, 0.7], ['20vh', '0vh']);
+  const phoneY = useTransform(scrollYProgress, [0.4, 0.7], ['0vh', '5vh']);
+  const phoneX = useTransform(scrollYProgress, [0.4, 0.7], ['130vw', '-40vw']);
+
 
   return (
     <section ref={containerRef} className="relative h-[150vh] bg-background">
       <div className="sticky top-0 h-screen w-full flex items-center justify-center overflow-hidden">
         <motion.div 
-            className="absolute w-[60vw] h-[60vh] max-w-[800px] max-h-[500px]"
+            className="absolute w-[100vw] h-[100vh] max-w-[1300px] max-h-[800px]"
             style={{
                 scale: laptopScale,
                 opacity: laptopOpacity,
@@ -34,7 +36,7 @@ const ParallaxImages = () => {
             }}
         >
           <Image
-            src="https://images.unsplash.com/photo-1678690832311-bb6e361989ca?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHx3ZWJzaXRlJTIwZGVzaWdufGVufDB8fHx8MTc2MTkzMTQwN3ww&ixlib=rb-4.1.0&q=80&w=1080"
+            src="/images/desarrollo web/2.png"
             alt="Laptop"
             fill
             className="object-contain"
@@ -43,16 +45,17 @@ const ParallaxImages = () => {
         </motion.div>
         
         <motion.div 
-            className="absolute w-[25vw] h-[50vh] max-w-[250px] max-h-[500px]"
+            className="absolute w-[100vw] h-[100vh] max-w-[600px] max-h-[1100px]"
              style={{
                 scale: phoneScale,
                 opacity: phoneOpacity,
                 y: phoneY,
+                x: phoneX,
                 zIndex: 20
             }}
         >
           <Image
-             src="https://images.unsplash.com/photo-1657812159075-7f0abd98f7b8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw2fHxlY29tbWVyY2UlMjB3ZWJzaXRlfGVufDB8fHx8MTc2MTg5MTYyN3ww&ixlib=rb-4.1.0&q=80&w=1080"
+             src="/images/desarrollo web/4.png"
             alt="Phone"
             fill
             className="object-contain"
