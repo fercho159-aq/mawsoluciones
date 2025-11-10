3697a3d4c234fe96ec4ee569b5b9020375bc1fb7
MAW SOLUCIONES <solucionesmaw@gmail.com>
2025-11-10 17:07:32 +0000

    sigue sin verse

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,11 +2,5 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
 
-const databaseUrl = process.env.DATABASE_URL;
-
-if (!databaseUrl) {
-  throw new Error("DATABASE_URL is not set in the environment variables");
-}
-
-const sql = neon(databaseUrl);
+const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle(sql, { schema });
