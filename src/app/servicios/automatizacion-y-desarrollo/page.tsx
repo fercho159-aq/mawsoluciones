15cd98e7f1c16fc211fbe3ce00a21912b6989b32
MAW SOLUCIONES <solucionesmaw@gmail.com>
2025-11-14 02:12:41 +0000

    esta en 3 columanasctualemnte , queiro este en 2 solamente.la caluclador

--- src/app/servicios/automatizacion-y-desarrollo/page.tsx
+++ src/app/servicios/automatizacion-y-desarrollo/page.tsx
@@ -39,18 +39,15 @@ const ServicePage = () => {
                 </li>
               </ul>
             </div>
-             <div className="grid lg:grid-cols-2 gap-12 items-center">
-              <DataAutomationCalculator />
-               <div className="relative aspect-[9/16] max-w-sm mx-auto w-full rounded-lg overflow-hidden shadow-xl">
-                 <iframe
-                  className="absolute top-0 left-0 w-full h-full"
-                  src="https://www.youtube.com/embed/UMw2F8fIvF4"
-                  title="YouTube video player"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                ></iframe>
-              </div>
+             <div className="relative aspect-[9/16] max-w-sm mx-auto w-full rounded-lg overflow-hidden shadow-xl">
+               <iframe
+                className="absolute top-0 left-0 w-full h-full"
+                src="https://www.youtube.com/embed/UMw2F8fIvF4"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
             </div>
           </div>
         </div>
--- src/app/servicios/creacion-de-contenido/page.tsx
+++ src/app/servicios/creacion-de-contenido/page.tsx
@@ -39,18 +39,15 @@ const ServicePage = () => {
                 </li>
               </ul>
             </div>
-             <div className="grid lg:grid-cols-2 gap-12 items-center">
-               <ContentRecommender />
-               <div className="relative aspect-[9/16] max-w-sm mx-auto w-full rounded-lg overflow-hidden shadow-xl">
-                 <iframe
-                  className="absolute top-0 left-0 w-full h-full"
-                  src="https://www.youtube.com/embed/t17eWu6lviA"
-                  title="YouTube video player"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                ></iframe>
-              </div>
+             <div className="relative aspect-[9/16] max-w-sm mx-auto w-full rounded-lg overflow-hidden shadow-xl">
+               <iframe
+                className="absolute top-0 left-0 w-full h-full"
+                src="https://www.youtube.com/embed/t17eWu6lviA"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
             </div>
           </div>
         </div>
